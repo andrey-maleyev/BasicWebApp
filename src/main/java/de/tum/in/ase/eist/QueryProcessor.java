dcd58e59df3df0d474d3ba621051f35b45587df6
Andrey Maleev <andrey.maleyev@gmail.com>
2022-07-14 13:19:49 +0200

    update

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,22 +12,11 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("what%20is%208%20plus%2017")) {
-           return "25";
-        } else if (query.contains("195")) {
-            return "195";
-        } else if (query.contains("415")) {
-            return "415";
-        } else if (query.contains("%20what%20is%209%20plus%200")) {
-            return "9";
-        } else if (query.contains("351")) {
-            return "351";
-        } else if (query.contains("757")) {
-            return "757";
-        } else if (query.contains("669")) {
-            return "669";
-        } else if (query.contains("802")) {
-            return "802";
+        } else if (query.contains("primes")) {
+            int index_a = query.indexOf("is%20", 0) + 1;
+            int index_b = query.indexOf("by%20", 0) + 1;
+            int a = Integer.parseInt(query.substring(index_a, index_a + 1));
+            int b = Integer.parseInt(query.substring(index_b, index_b + 1));
         } else { // TODO extend the programm here
             return "";
         }
